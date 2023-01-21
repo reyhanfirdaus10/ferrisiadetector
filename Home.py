d6650de2a3a34a8842a3ba576a3cfdf6cab0d3a3
M. Reyhan Firdaus <72717109+reyhanfirdaus10@users.noreply.github.com>
2023-01-21 15:57:56 +0700

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,45 @@
+import streamlit as st
+
+hidemenu =  """
+<style>
+    #MainMenu{
+        visibility:hidden;
+    }
+    footer{
+        visibility:visible;
+    }
+    footer:after{
+        content:'Copyright © 2022 Universitas Bengkulu';
+        display:block;
+        position:relative;
+        color:tomato;
+    }
+</style>
+"""
+
+
+st.markdown("# **:blue[Bacteria]** Colony Counter")
+st.write(
+    """Web app to counting bacteria colony automatically – easy to use and fast."""
+)
+link = '[Start Counting Colony](https://harizaldycahya-colony-counter-home-ng37y9.streamlit.app/Counting)'
+st.markdown(link, unsafe_allow_html=True)
+
+
+st.markdown(
+    """
+        <style>
+            @font-face {
+            font-family: 'Poppins';
+            src: url(https://fonts.googleapis.com/css2?family=Poppins&display=swap);
+            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+            }
+
+            html, body, [class*="css"]  {
+                font-family: 'Poppins';
+            }
+        </style>
+
+    """,
+        unsafe_allow_html=True,
+)
